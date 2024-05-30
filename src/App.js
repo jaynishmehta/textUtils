5d0507ed774a236f76f64200605d40f3c9dcd69a
JAYNISH MEHTA <bondt929@gmail.com>
2024-05-31 00:13:16 +0530

    enable dark mode button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForms from './components/TextForms';
-
-
+// import TextForms from './components/TextForms';
+import About from './components/About';
 export default function App() {
   return (
     <>
@@ -13,7 +12,8 @@ export default function App() {
     <Navbar title={"TextUtils"} /> 
     {/* using container divsion built in div in boots for styling */}
     <div className="container my-3"> 
-       <TextForms heading="Enter the text to analyse"/> 
+       {/* <TextForms heading="Enter the text to analyse"/>  */}
+       <About/>
     </div>
     
   </>
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -1,7 +1,8 @@
 import React,{useState} from 'react'
 
 export default function TextForms(props) {
-    const handleUpClick= ()=>{
+    const handleUpClick= (event)=>{
+        event.preventDefault();
         console.log("Uppercase was clicked"+text);
         let newtext= text.toUpperCase();
         setText(newtext);
@@ -10,7 +11,18 @@ export default function TextForms(props) {
         console.log("Onchange");
         setText(event.target.value);
     }
-
+    
+    const handleReverse= (event)=>{
+        event.preventDefault(); //this helped the page to not to change to the default state
+        console.log("clicked on reverse");
+        
+        function reverseString(newtext) {
+          newtext = newtext.split("").reverse().join("");
+          return newtext;
+        }
+        let newtext = text;
+        setText(reverseString(newtext))
+    }
     const[text,setText]= useState('Enter your text');
     // text="new-text";// wrong way to change the state
     // setText("new text"); // correct way to set the state
@@ -21,7 +33,8 @@ export default function TextForms(props) {
         <div className="form-group">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary mx-2" onClick={handleReverse}>Convert to Reverse</button>
     </form>
     </div>
   )
